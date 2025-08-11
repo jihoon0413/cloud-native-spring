2dfcb2fe161a1f55d83495693a19c31c3040f8b7
jihoon <wlgnstls0413@naver.com>
2025-08-11 11:14:58 +0900

    CH4.1-사용자 정의 속성 활용
    polar: greeting를 application.yml에 추가하고
    homeController에서 polar.greeting 속성을 활용 예시

--- catalog-service/src/main/java/com/polarbookshop/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/polarbookshop/catalog_service/CatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@SpringBootApplication
 @ConfigurationPropertiesScan  // 스프링 콘텍스트에 설정 데이터를 빈을 로드한다.
 //@EnableConfigurationProperties(PolarProperties.class)  // EnableConfigurationProperties 로 직접 설정도 가능
 public class CatalogServiceApplication {
