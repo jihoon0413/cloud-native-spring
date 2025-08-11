0bb7adc498d2271e0e809252351a50f68b6140c4
jihoon <wlgnstls0413@naver.com>
2025-08-11 11:06:26 +0900

    CH3.4-@WebMvcTest 를 활용한 슬라이스 테스트
    단위와 통합 테스트의 중간 단계

---
+++ catalog-service/src/test/java/com/polarbookshop/catalog_service/web/BookControllerMvcTests.java
@@ -0,0 +1,37 @@
+package com.polarbookshop.catalog_service.web;
+
+import com.polarbookshop.catalog_service.domain.BookNotFoundException;
+import com.polarbookshop.catalog_service.domain.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
+public class BookControllerMvcTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private BookService bookService;
+
+    @Test
+    void whenGetBookNotExistingThenShouldReturn404() throws Exception {
+        String isbn = "73737313940";
+        when(bookService.viewBookDetails(isbn)).thenThrow(new BookNotFoundException(isbn));
+        mockMvc.perform(get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+
+//        given(bookService.viewBookDetails(isbn))
+//                .willThrow(BookNotFoundException.class);
+//        mockMvc.perform(get("/books/" + isbn))
+//                .andExpect(status().isNotFound());
+    }
+
+}
