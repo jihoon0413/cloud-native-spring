a94baa0256893e8a5feeb9302c128564efdc3165
jihoon <wlgnstls0413@naver.com>
2025-08-11 11:09:00 +0900

    CH4.1-사용자 지성 속정 정의
    설정 데이터 빈 스캔 활성화

--- catalog-service/src/main/java/com/polarbookshop/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/polarbookshop/catalog_service/CatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.polarbookshop.catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@ConfigurationPropertiesScan  // 스프링 콘텍스트에 설정 데이터를 빈을 로드한다.
+//@EnableConfigurationProperties(PolarProperties.class)  // EnableConfigurationProperties 로 직접 설정도 가능
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
