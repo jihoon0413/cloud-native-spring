1098027029bce9331eff94af55032cb5122ddbdb
jihoon <wlgnstls0413@naver.com>
2025-08-14 15:21:39 +0900

    CH5.2-데이터 Repository
    CrudRepository를 상속하여 postgres 의 데이터에 액세스
    
    기존의 InMemoryBookRepository 삭제,
    @Transactional 적용

--- catalog-service/src/main/java/com/polarbookshop/catalog_service/demo/BookDataLoader.java
+++ catalog-service/src/main/java/com/polarbookshop/catalog_service/demo/BookDataLoader.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Profile("testdata")  // testdata 프로파일이 활성활 될 때만 로드 됨
 public class BookDataLoader {
@@ -18,10 +20,10 @@ public class BookDataLoader {
 
     @EventListener(ApplicationReadyEvent.class) // ApplicationReadyEvent가 발생하면 다음 메소드(테스트 데이터 생성)를 실행
     public void loadBookTestData() {
+        bookRepository.deleteAll();
         var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
         var book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        bookRepository.saveAll(List.of(book1, book2));
     }
 
 }
--- catalog-service/src/main/java/com/polarbookshop/catalog_service/domain/BookRepository.java
+++ catalog-service/src/main/java/com/polarbookshop/catalog_service/domain/BookRepository.java
@@ -1,12 +1,20 @@
 package com.polarbookshop.catalog_service.domain;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
-public interface BookRepository {
-    Iterable<Book> findAll();
+public interface BookRepository extends CrudRepository<Book, Long> {
+
     Optional<Book> findByIsbn(String isbn);
     boolean existsByIsbn(String isbn);
-    Book save(Book book);
+
+    @Modifying // 데이터베이스 상태를 수정할 연산임을 나타냄 @LastModifiedDate를 활성화하기 위해 사용
+    @Transactional
+    @Query("delete from Book where isbn = :isbn") // jdbc에서는 필드 이름으로 매핑해주는 기능이 없어 query를 적어줘야함
     void deleteByIsbn(String isbn);
 
 }
