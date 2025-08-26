242e04c85827eb43ffc7e51d30554ddfa59596f0
jihoon <wlgnstls0413@naver.com>
2025-08-26 21:14:43 +0900

    CH8.2-비즈니스 로직 구현
    주문 요청의 경우 일단 rejected 상태로만 리턴하도록 설정

---
+++ order-service/src/main/java/com/polarbookshop/order_service/order/web/OrderController.java
@@ -0,0 +1,31 @@
+package com.polarbookshop.order_service.order.web;
+
+import com.polarbookshop.order_service.order.domain.Order;
+import com.polarbookshop.order_service.order.domain.OrderService;
+import com.polarbookshop.order_service.order.domain.OrderStatus;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public Flux<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
+        return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
+    }
+
+}
