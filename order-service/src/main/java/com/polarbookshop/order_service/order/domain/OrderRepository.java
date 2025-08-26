b5f813fa27339b6f86e6fc67e6d14f19397d59d2
jihoon <wlgnstls0413@naver.com>
2025-08-26 21:06:04 +0900

    CH8.2-리액티브 repository 사용

---
+++ order-service/src/main/java/com/polarbookshop/order_service/order/domain/OrderRepository.java
@@ -0,0 +1,6 @@
+package com.polarbookshop.order_service.order.domain;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface OrderRepository extends ReactiveCrudRepository<Order, Long> {
+}
