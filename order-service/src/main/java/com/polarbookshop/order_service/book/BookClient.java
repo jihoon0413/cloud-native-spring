6121dca31719a179919e645ecfba3d044d7b3d40
jihoon <wlgnstls0413@naver.com>
2025-08-28 10:22:55 +0900

    CH8.4-복원력 높은 애플리테이션 설정
    catalog-service라는 다른 서비스에 연결할 때 타임아웃, 재시도, 풀백 및 오류 처리

--- order-service/src/main/java/com/polarbookshop/order_service/book/BookClient.java
+++ order-service/src/main/java/com/polarbookshop/order_service/book/BookClient.java
@@ -2,7 +2,11 @@ package com.polarbookshop.order_service.book;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 public class BookClient {
@@ -18,7 +22,15 @@ public class BookClient {
                 .get()
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve() // 요청을 보내고 응답을 받는다.
-                .bodyToMono(Book.class);
+                .bodyToMono(Book.class)
+                .timeout(Duration.ofSeconds(3), Mono.empty())  // get 요청에 대해 3초 타임아웃 설정, 에러 대신 풀백으로 mono 빈객체 반환
+                .onErrorResume(WebClientResponseException.NotFound.class,
+                        e -> Mono.empty())
+                .retryWhen(
+                        Retry.backoff(3, Duration.ofMillis(100))
+                )
+                .onErrorResume(Exception.class,
+                        e -> Mono.empty());
     }
 
 }
