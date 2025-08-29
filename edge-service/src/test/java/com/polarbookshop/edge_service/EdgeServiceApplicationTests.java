3f7411b6551f6560822f27c86c25c24e0682287c
jihoon <wlgnstls0413@naver.com>
2025-08-29 22:00:48 +0900

    CH9.4-레디스를 통한 분산 세션 관리
    spring-session-data-redis 사용

--- edge-service/src/test/java/com/polarbookshop/edge_service/EdgeServiceApplicationTests.java
+++ edge-service/src/test/java/com/polarbookshop/edge_service/EdgeServiceApplicationTests.java
@@ -2,9 +2,33 @@ package com.polarbookshop.edge_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Testcontainers
 class EdgeServiceApplicationTests {
+
+	private static final int REIDS_PORT = 6379;
+
+	@Container
+	static GenericContainer<?> redis =
+			new GenericContainer<>(DockerImageName.parse("redis:7.0"))
+			.withExposedPorts(REIDS_PORT);
+
+	@DynamicPropertySource
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.redis.host",
+				() -> redis.getHost());
+		registry.add("spring.data.redis.port",
+				() -> redis.getMappedPort(REIDS_PORT));
+	}
 
 	@Test
 	void contextLoads() {
