a37422424d43c23d0a67b1d3d40fd4e935e78158
jihoon <wlgnstls0413@naver.com>
2025-09-05 14:35:19 +0900

    CH10-통합테스트에 @DIsabled
    binder가 없을 때만 실행 가능
    자세한 이유 찾아보기

--- dispatcher-service/src/test/java/com/polarbookshop/dispatcher_service/DispatchingFunctionsIntegrationTests.java
+++ dispatcher-service/src/test/java/com/polarbookshop/dispatcher_service/DispatchingFunctionsIntegrationTests.java
@@ -1,15 +1,18 @@
 package com.polarbookshop.dispatcher_service;
 
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.function.context.FunctionCatalog;
-import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
-import java.util.function.Function;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.function.context.FunctionCatalog;
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
 
 @FunctionalSpringBootTest
+@Disabled("These tests are only necessary when using the functions alone (no bindings)")
 public class DispatchingFunctionsIntegrationTests {
 
     @Autowired
@@ -19,13 +22,10 @@ public class DispatchingFunctionsIntegrationTests {
     void packAndLabelOrder() {
         Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>>
                 packAndLabel = catalog.lookup(
-                        Function.class,
-                "pack|label"); //FunctionCatalog로부터 합성 함수를 가져옴
+                        Function.class, "pack|label"); //FunctionCatalog로부터 합성 함수를 가져옴
         long orderId = 121;
 
-        StepVerifier.create(packAndLabel.apply( // 함수 자체가 자체가 올바르게 동작하는지 확인
-                new OrderAcceptedMessage(orderId) // 함수에 대한 입력 정의
-        ))
+        StepVerifier.create(packAndLabel.apply(new OrderAcceptedMessage(orderId)))
                 .expectNextMatches(dispatchedOrder ->  // 출력 결과가 일지하는지 확인
                         dispatchedOrder.equals(new OrderDispatchedMessage(orderId)))
                 .verifyComplete();
