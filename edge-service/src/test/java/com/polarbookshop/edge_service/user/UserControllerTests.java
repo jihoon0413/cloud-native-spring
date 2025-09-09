9e83410509697cb73ba0091a8f6a275e4f49e393
jihoon <wlgnstls0413@naver.com>
2025-09-09 13:13:33 +0900

    CH11.6- OIDC 인증 테스트

---
+++ edge-service/src/test/java/com/polarbookshop/edge_service/user/UserControllerTests.java
@@ -0,0 +1,67 @@
+package com.polarbookshop.edge_service.user;
+
+import com.polarbookshop.edge_service.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebFluxTest(UserController.class)
+@Import(SecurityConfig.class)
+public class UserControllerTests {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @MockitoBean
+    ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+    @Test
+    void whenNotAuthenticatedThen401() {
+        webTestClient
+                .get()
+                .uri("/user")
+                .exchange()
+                .expectStatus().isUnauthorized();
+    }
+
+    @Test
+    void whenAuthenticatedThenReturnUser() {
+        var expectedUser = new User("jon.snow", "Jon", "Snow",
+                List.of("employee", "customer"));
+
+        webTestClient
+                .mutateWith(configureMockOidcLogin(expectedUser))
+                .get()
+                .uri("/user")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(User.class)
+                .value(user -> assertThat(user).isEqualTo(expectedUser));
+
+    }
+
+    private SecurityMockServerConfigurers.OidcLoginMutator configureMockOidcLogin(User expectedUser) {
+        return SecurityMockServerConfigurers.mockOidcLogin().idToken(
+                builder -> {  // 모의 id 토큰 생성
+                    builder.claim(StandardClaimNames.PREFERRED_USERNAME,
+                            expectedUser.userName());
+                    builder.claim(StandardClaimNames.GIVEN_NAME,
+                            expectedUser.firstName());
+                    builder.claim(StandardClaimNames.FAMILY_NAME,
+                            expectedUser.lastName());
+
+                });
+    }
+
+}
